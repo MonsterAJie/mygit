3002a08357188afee4739215ed160127443badb3
BigOfRice <294420785@qq.com>
2018-10-29 16:09:19 +0800

    atom manipulation test
    Signed-off-by: BigOfRice <294420785@qq.com>

---
+++ ConcurrencyTest/Thread_Test/src/com/jie/day_02/AtomManipulationTest.java
@@ -0,0 +1,74 @@
+package com.jie.day_02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.w3c.dom.css.Counter;
+
+public class AtomManipulationTest {
+	
+	private AtomicInteger atomicI = new AtomicInteger(0);
+	
+	private int i = 0;
+	/**
+	 * volatile保证可见性，不能保证原子性
+	 */
+	private volatile int j = 0;
+	
+	public static void main(String[] args) {
+		final AtomManipulationTest cas = new AtomManipulationTest();
+		List<Thread> ts = new ArrayList<Thread>(600);
+		long start = System.currentTimeMillis();
+		for (int j = 0; j < 100; j ++) {
+			Thread t = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					for (int i = 0; i < 10000; i ++) {
+						cas.count();
+						cas.safeCount();
+					}
+					
+				}	
+				
+			});
+			ts.add(t);
+		}
+		for (Thread t : ts) {
+			t.start();
+		}
+		//等待所有线程执行完成
+		for (Thread t : ts) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(cas.i);
+		System.out.println(cas.atomicI.get());
+		System.out.println(cas.j);
+		System.out.println(System.currentTimeMillis() - start);
+	}
+	/**
+	 * 使用cas实现线程安全计数器
+	 */
+	private void safeCount() {
+		for (;;) {
+			int i = atomicI.get();
+			boolean suc = atomicI.compareAndSet(i, ++i);
+			if (suc) {
+				break;
+			}
+		}
+	}
+	/**
+	 * 费线程安全计数器
+	 */
+	private void count() {
+		i++;
+		j++;
+	}
+
+}
